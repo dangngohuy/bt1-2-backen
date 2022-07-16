ee3d6b103637f11620006045636d3ab553d1d629
dangngohuy <109237240+dangngohuy@users.noreply.github.com>
2022-07-16 20:50:36 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./routes/contact.route");
 
 const app = express();
 
 app . use(cors());
 app .use(express.json());
-
+ 
 app .get('/' , (req ,res) => {
     res .json ({message: ' xin chao den voi ung ung quan ly danh ba!'});
 });
+app.use("/api/contacts" ,contactsRouter);
+
 
 module .exports = app;
